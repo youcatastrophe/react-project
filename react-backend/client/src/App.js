6494b9994e20351d702db503343c7116c5f18e9f
Ryan Newell <ryanalbertnewell@gmail.com>
2018-06-08 11:07:40 -0500

    Trying to resolve proxy issue.

---
+++ react-backend/client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = { users: [] }
+
+  componentDidMount(){
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
